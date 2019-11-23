a229b0772ca8f749cb0013919999f862c5055b23
Reduse-git <den.reduse@gmail.com>
2019-11-23 10:18:51 +0200

    Do something

--- app/src/main/java/com/reduse/quations/MainActivity.java
+++ app/src/main/java/com/reduse/quations/MainActivity.java
@@ -1,14 +1,73 @@
 package com.reduse.quations;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView questionString = findViewById(R.id.question_string);
+        final Question currentQuestion = mQuestionBank[mCurrentIndex];
+        questionString.setText(currentQuestion.getQuestionResId());
+
+        Button trueButton = findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(true);
+            }
+        });
+
+        Button falseButton = findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(false);
+            }
+        });
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+
+                final Question currentQuestion = mQuestionBank[mCurrentIndex];
+                questionString.setText(currentQuestion.getQuestionResId());
+            }
+        });
+    }
+
+    private void onButtonClicked(boolean answer) {
+        Question currentQuestion = mQuestionBank[mCurrentIndex];
+        int toastMessage = (currentQuestion.isCorrectAnswer() == answer) ?
+                R.string.correct_toast :
+                R.string.incorrect_toast;
+
+        Toast.makeText(
+                MainActivity.this,
+                toastMessage,
+                Toast.LENGTH_LONG
+        ).show();
     }
 }
---
+++ app/src/main/java/com/reduse/quations/Question.java
@@ -0,0 +1,19 @@
+package com.reduse.quations;
+
+public class Question {
+    private int questionResId;
+    private boolean correctAnswer;
+
+    public Question(int questionResId, boolean correctAnswer) {
+        this.questionResId = questionResId;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getQuestionResId() {
+        return questionResId;
+    }
+
+    public boolean isCorrectAnswer() {
+        return correctAnswer;
+    }
+}
